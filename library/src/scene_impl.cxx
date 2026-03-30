4434d753a7728ba1ad257983b4ae514b90b9cdab
Kirill Adamov <highwayman180298@gmail.com>
2026-03-30 23:52:05 +0300

    feature: native parsing
    fix typo, remove _option_default_value_start and -/-end
    
    fix namespaces in cmake func; add default values to options.json; reimplement get optional color for animation and loader progress bars
    
    fix: remove useless dependency added in previous commit
    
    fix after review - remove logic for optional
    
    remove odd strs
    
    fix style
    
    update doc

--- library/src/scene_impl.cxx
+++ library/src/scene_impl.cxx
@@ -119,16 +119,7 @@ public:
     callbackData.widget = progressWidget;
     if (this->Options.ui.loader_progress && this->Interactor)
     {
-      f3d::color_t color;
-      if (!this->Options.ui.loader_progress_color.has_value())
-      {
-        const auto [r, g, b] = F3DStyle::GetF3DYellow();
-        color = color_t(r, g, b);
-      }
-      else
-      {
-        color = this->Options.ui.loader_progress_color.value();
-      }
+      f3d::color_t color = this->Options.ui.loader_progress_color;
       scene_impl::internals::CreateProgressRepresentationAndCallback(
         &callbackData, this->MetaImporter, this->Interactor, color);
     }
